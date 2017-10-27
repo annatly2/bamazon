b55932c49c85df20c6e91d953b4c8043e34bbedb
Anna Ly <annatly2@gmail.com>
2017-10-27 00:02:49 -0700

    oct 27

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,16 +17,16 @@ connection.connect(function(err){
   initialPrompt();
 });
 
-function displayItems(){
-  console.log("Displaying all items available...\n");
-  connection.query("SELECT * FROM products", function (err, res){
-    if (err) throw err;
-    for(i = 0; i < res.length; i++){
-      console.log("(Id# " + res[i].item_id + ") " + res[i].product_name + " $" + res[i].price);
-    }
-    endConnection();
-  });
-}
+// function displayItems(){
+//   console.log("Displaying all items available...\n");
+//   connection.query("SELECT * FROM products", function (err, res){
+//     if (err) throw err;
+//     for(i = 0; i < res.length; i++){
+//       console.log("\n(Id# " + res[i].item_id + ") " + res[i].product_name + " $" + res[i].price);
+//     }
+//     endConnection();
+//   });
+// }
 
 
 
@@ -38,32 +38,60 @@ function displayItems(){
 //if not-> prompt insufficient quantity
 
 // if so -> fulfill customer's orders
-	//updating sql database to reflect remaining quanity
+	//updating sql database to reflect remaining quantity
 //when order goes through, show customer total cost of their purchase
+//thank you for your purchase
 
 
 
 function initialPrompt() {
+
+  console.log("Displaying all items available...\n");
+  connection.query("SELECT * FROM products", function (err, res){
+
+    if (err) throw err;
+
+    for(i = 0; i < res.length; i++){
+      var productID = res[i].item_id;
+      var productName = res[i].product_name;
+      var productPrice = res[i].price;
+      console.log("\n(Id# " + productID+ ") " + productName+ " $" + productPrice);
+    }
+    //endConnection();
+
   inquirer
-    .prompt({
-      name: "id",
-      type: "input",
-      message: "Enter the ID # (just the number) of the product you would like to buy"
-    },{
-      name: "quantity",
-      type: "input",
-      message: "How many would you like of this product?"
-    })
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "Enter the ID # (1 through 10) of the product you would like to buy"
+      },
+      {
+        name: "unit",
+        type: "input",
+        message: "How many would you like to buy?"
+      }
+    ])
     .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid.toUpperCase() === "POST") {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
+      var idWanted = answer.id;
+      var unitWanted = answer.unit;
+      var newIndex = parseInt(idWanted - 1);
+
+      console.log("Id" + idWanted);
+      console.log("Unit: "+ unitWanted);
+
+      console.log(res[newIndex].item_id);
+      console.log(res[newIndex].product_name);
+
+
+
+    //assign this id to an id from the mysql list
+      
     });
+
+  });
 }
+
 
 
 function endConnection(){
